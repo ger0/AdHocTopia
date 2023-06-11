6c9fae23fc10b80fdad9da6ad46a96ef1d069ab9
ger0 <mrgero155@gmail.com>
2023-06-11 03:34:15 +0200

    Ad-Hoc setup working

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <vector>
 #include <array>
 #include <fmt/ranges.h>
@@ -5,13 +6,19 @@
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <linux/wireless.h>
 
-using uint = unsigned int;
-using byte = unsigned char;
+using uint  = unsigned int;
+using byte  = unsigned char;
+using c_str = const char*;
 
 constexpr uint PORT = 2113;
 constexpr uint MAX_BUFFER_SIZE = 1024;
 
+struct Map {
+    static constexpr uint WIDTH     = 800;
+    static constexpr uint HEIGHT    = 600;
+} map;
 
 #define LOG(...) \
     fmt::print(stdout, "{}\n", fmt::format(__VA_ARGS__))
@@ -25,21 +32,49 @@ constexpr uint MAX_BUFFER_SIZE = 1024;
 #define LOG_DBG(...) ;
 #endif
 
-bool create_adhoc_network(const char* device, const char* ip_addr) {
+#ifndef defer
+struct defer_dummy {};
+template <class F> struct deferrer { F f; ~deferrer() { f(); } };
+template <class F> deferrer<F> operator*(defer_dummy, F f) { return {f}; }
+#define DEFER_(LINE) zz_defer##LINE
+#define DEFER(LINE) DEFER_(LINE)
+#define defer auto DEFER(__LINE__) = defer_dummy{} *[&]()
+#endif // defer
+
+bool create_adhoc_network(c_str device, c_str essid, c_str ip_addr) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) {
         LOG_ERR("Failed to create socket");
         return false;
     }
-    auto fail = [&sock]() {close(sock); return false;};
-
-    struct ifreq ifr;
+    // close socket opon returning
+    defer {close(sock);};
+    
+    struct ifreq ifr; // interface settings
+    struct iwreq iwr; // wireless  settings
     memset(&ifr, 0, sizeof(ifr));
+    memset(&iwr, 0, sizeof(iwr));
+    strncpy(ifr.ifr_name, device, IFNAMSIZ);
+    strncpy(iwr.ifr_ifrn.ifrn_name, device, IFNAMSIZ);
 
-    strncpy(ifr.ifr_name, device, IFNAMSIZ - 1);
     if (ioctl(sock, SIOCGIFFLAGS, &ifr)) {
         LOG_ERR("Failed to retrieve interface flags");
-		return fail();
+        return false;
+    }
+
+    memset(&iwr.u, 0, sizeof(iwr.u));
+    iwr.u.mode |= IW_MODE_ADHOC;
+    if (ioctl(sock, SIOCSIWMODE, &iwr)) {
+        LOG_ERR("Failed to set the wireless interface to ad-hoc mode");
+        return false;
+    }
+
+    iwr.u.essid.pointer = (caddr_t)essid;
+    iwr.u.essid.length = strlen(essid);
+    iwr.u.essid.flags = 1;
+    if (ioctl(sock, SIOCSIWESSID, &iwr)) {
+        LOG_ERR("Failed to set the ESSID");
+        return false;
     }
 
     // set up the ip address 
@@ -49,38 +84,38 @@ bool create_adhoc_network(const char* device, const char* ip_addr) {
     sock_addr.sin_port = 0;
     if (inet_pton(AF_INET, ip_addr, &(sock_addr.sin_addr)) <= 0) {
         LOG_ERR("Invalid IP address");
-		return fail();
+		return false;
     }
     memcpy(&ifr.ifr_addr, &sock_addr, sizeof(sock_addr));
     if (ioctl(sock, SIOCSIFADDR, &ifr)) {
         LOG_ERR("Failed to set interface IP address");
-		return fail();
+		return false;
     }
     if (ioctl(sock, SIOCGIFFLAGS, &ifr)) {
         LOG_ERR("Failed to retrieve interface flags");
-		return fail();
+		return false;
     }
 
     // set the interface UP
     ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
     if (ioctl(sock, SIOCSIFFLAGS, &ifr)) {
         LOG_ERR("Failed to set interface flags");
-		return fail();
+		return false;
     }
     LOG_DBG("Ad hoc network created successfully!");
-    close(sock); 
     return true;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        LOG("Usage: {} <device> <ip_address>", argv[0]);
+    if (argc != 4) {
+        LOG("Usage: {} <device> <essid> <ip_address>", argv[0]);
         return EXIT_FAILURE;
     }
-    const char* device  = argv[1];
-    const char* ip_addr = argv[2];
+    c_str device    = argv[1];
+    c_str essid     = argv[2];
+    c_str ip_addr   = argv[3];
 
-    if (!create_adhoc_network(device, ip_addr)) {
+    if (!create_adhoc_network(device, essid, ip_addr)) {
         perror("What");
         return EXIT_FAILURE;
     }
